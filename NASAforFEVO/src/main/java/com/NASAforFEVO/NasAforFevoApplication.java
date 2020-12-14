167ab1c00c2280cb6d39fc5dafc9cb17f01a9cf3
harsh0914 <saini.harshveer@gmail.com>
2020-12-14 03:37:20 -0800

    Changed Annotation to SpringBootApplication.

--- NASAforFEVO/src/main/java/com/NASAforFEVO/NasAforFevoApplication.java
+++ NASAforFEVO/src/main/java/com/NASAforFEVO/NasAforFevoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@SpringBootConfiguration
+@SpringBootApplication
 public class NasAforFevoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NasAforFevoApplication.class, args);
